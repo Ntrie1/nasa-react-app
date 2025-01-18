9fd22d8d2351e57659aadaa65aa9d470500717d6
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-18 16:19:44 +0200

    api get func

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 function App() {
   const [showModal, setShowModel] = useState(false);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    const fetchAPIData = async () => {
+      const NASA_KEY = import.meta.env.VITE_NASA_API;
+      const url = `https://api.nasa.gov/planetary/apod` + `?api_key=${NASA_KEY}`;
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        setData(apiData);
+        
+      } catch (error) {
+        console.log(error.message);
+        
+      }
+      
+      
+    }
+    fetchAPIData();
+  },[])
 
   const handleDisplayModel = () => {
     setShowModel((prev) => !prev);
@@ -12,7 +34,11 @@ function App() {
 
   return (
     <>
-      <Main />
+      {data ? (<Main />) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
      { showModal && (<SideBar handleDisplayModel={handleDisplayModel} />)}
       <Footer handleDisplayModel={handleDisplayModel}  />
     </>
