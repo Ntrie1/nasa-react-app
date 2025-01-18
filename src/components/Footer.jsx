6ca332f07d3d6ef69743e0188d1ca75aef5145bd
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-18 17:43:10 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer({handleDisplayModel, data}) {
         <footer>
             <div className='bgGradient'></div>
             <div>
-                <h2>{data?.title}</h2>
                 <h1>Glaxio PROJECT</h1>
+                <h2>{data?.title}</h2>
             </div>
             <button onClick={handleDisplayModel}>
                 <i className="fa-solid fa-info"></i>
