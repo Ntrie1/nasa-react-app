bf3844b3be261a0ea541bb3a068496c04bc70d63
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-18 13:55:43 +0200

    footer css added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 
 export default function Footer() {
-  return (
-    <footer>
-        <div>
-            <h2>
-                The Brutal Marsian Landscape
-            </h2>
-            <h1>
-                Glaxio PROJECT
-            </h1>
+    return (
+        <footer>
+            <div className='bgGradient'></div>
+            <div>
+                <h2>The Brutal Marsian Landscape</h2>
+                <h1>Glaxio PROJECT</h1>
+            </div>
             <button>
-            <i className="fa-solid fa-info"></i>
+                <i className="fa-solid fa-info"></i>
             </button>
-        </div>
-    </footer>
-  )
+
+        </footer>
+    )
 }
