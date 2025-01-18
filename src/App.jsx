22044fc7bac49b01e919a0f67d218349d834de08
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-18 17:44:08 +0200

    persist in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,31 @@ import SideBar from "./components/SideBar"
 function App() {
   const [showModal, setShowModel] = useState(false);
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false)
+  // const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchAPIData = async () => {
       const NASA_KEY = import.meta.env.VITE_NASA_API;
       const url = `https://api.nasa.gov/planetary/apod` + `?api_key=${NASA_KEY}`;
 
+
+      const today = (new Date()).toDateString();
+      const localKey = `NASA-${today}`;
+
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        return;
+      };
+
+      localStorage.clear();
+
+
       try {
         const res = await fetch(url);
         const apiData = await res.json();
         setData(apiData);
+        localStorage.setItem(localKey, JSON.stringify(apiData));
 
       } catch (error) {
         console.log(error.message);
@@ -34,17 +48,17 @@ function App() {
 
   return (
     <>
-      {data ? (<Main />) : (
+      {data ? (<Main data={data} />) : (
         <div className="loadingState">
           <i className="fa-solid fa-gear"></i>
         </div>
       )}
 
-      {showModal && 
-      (<SideBar data={data} handleDisplayModel={handleDisplayModel} />)}
+      {showModal &&
+        (<SideBar data={data} handleDisplayModel={handleDisplayModel} />)}
 
       {data &&
-       (<Footer data={data} handleDisplayModel={handleDisplayModel} />)}
+        (<Footer data={data} handleDisplayModel={handleDisplayModel} />)}
     </>
   )
 }
